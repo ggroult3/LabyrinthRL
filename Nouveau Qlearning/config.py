fb153eb2952efadcd9c4e1a96496c331e6346be8
Antoine Valentin <45234999+Orizno@users.noreply.github.com>
2021-02-04 11:51:17 +0100

    Rajout de la gestion de l'eau
    Marche très mal

--- Nouveau Qlearning/config.py
+++ Nouveau Qlearning/config.py
@@ -3,7 +3,7 @@ import torch
 gamma = 0.99
 batch_size = 32
 lr = 0.001
-initial_exploration = 10000
+initial_exploration = 300
 goal_score = 200
 log_interval = 10
 update_target = 100
--- Nouveau Qlearning/train.py
+++ Nouveau Qlearning/train.py
@@ -3,7 +3,7 @@ import sys
 import gym
 import random
 import numpy as np
-
+from copy import deepcopy
 import torch
 import torch.optim as optim
 import torch.nn.functional as F
@@ -15,46 +15,53 @@ from display import Displayer
 
 from config import initial_exploration, batch_size, update_target, goal_score, log_interval, device, replay_memory_capacity, lr
 
-def get_action(state, target_net, epsilon, env,test=False):
-    actions_list=[[1,0],[-1,0],[0,1],[0,-1]]
-    if np.random.rand() <= epsilon and test==False: #au hasard
-        action=torch.tensor([[random.randrange(4)]], device=device, dtype=torch.long)
-        newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
-        if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]!=0:
-            return action
-        while env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]==0:
+def get_action(state, target_net, epsilon, env,test=False,seprendrelesmurs=True):
+    if not seprendrelesmurs:
+        actions_list=[[1,0],[-1,0],[0,1],[0,-1]]
+        if np.random.rand() <= epsilon and test==False: #au hasard
             action=torch.tensor([[random.randrange(4)]], device=device, dtype=torch.long)
             newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
-        return action #Renvoie ici un tenseur valant de 0 à 3, qui n'entraîne pas un déplacement vers un mur
+            if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]!=0:
+                return action
+            while env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]==0:
+                action=torch.tensor([[random.randrange(4)]], device=device, dtype=torch.long)
+                newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
+            return action #Renvoie ici un tenseur valant de 0 à 3, qui n'entraîne pas un déplacement vers un mur
+        else:
+            qvalue=target_net.get_action(state)
+            listeindice=qvalue.topk(4)[1][0]
+            action=listeindice[0].cpu().numpy()
+            newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
+            if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]!=0:
+                return action
+            action=listeindice[1].cpu().numpy()
+            newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
+            if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]!=0:
+                return action
+            action=listeindice[2].cpu().numpy()
+            newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
+            if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]!=0:
+                return action
+            action=listeindice[3].cpu().numpy()
+            return action
+
+    if np.random.rand() <= epsilon and test==False:
+        return torch.tensor([[random.randrange(4)]], device=device, dtype=torch.long)
     else:
-        qvalue=target_net.get_action(state)
-        listeindice=qvalue.topk(4)[1][0]
-        action=listeindice[0].cpu().numpy()
-        newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
-        if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]!=0:
-            return action
-        action=listeindice[1].cpu().numpy()
-        newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
-        if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]!=0:
-            return action
-        action=listeindice[2].cpu().numpy()
-        newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
-        if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]!=0:
-            return action
-        action=listeindice[3].cpu().numpy()
-        return action
+        _, action = torch.max(target_net.get_action(state), 1)
+        return action.cpu().numpy()[0]
 
 def update_target_model(online_net, target_net):
     # Target <- Net
     target_net.load_state_dict(online_net.state_dict())
 
 
-def main(L, mouse_initial_indices, rewardlist, actions_list):
+def main(L, mouse_initial_indices, rewardlist, actions_list,seprendrelesmurs=True):
     # L=np.array([[0,0,2,0,0,0,0],[0,1,1,0,1,1,0],[0,1,0,0,1,0,0],[0,1,1,1,1,0,0],[0,0,1,0,1,1,0],[0,0,0,0,0,1,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,0]])
     # mouse_initial_indices=[5,5]
 
 
-    env = L
+    env = deepcopy(L)
     torch.manual_seed(500)
 
     num_inputs = 2
@@ -79,9 +86,9 @@ def main(L, mouse_initial_indices, rewardlist, actions_list):
     steps = 0
     loss = 0
 
-    for e in range(250):
+    for e in range(10000):
         done = False
-
+        env = deepcopy(L)
         score = 0
         state = np.array(mouse_initial_indices)
         state = torch.Tensor(state).to(device)
@@ -97,8 +104,9 @@ def main(L, mouse_initial_indices, rewardlist, actions_list):
                 reward=rewardlist[env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]]
                 if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]==2:
                     done=True
+                if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]==4: #if the mouse is in the water
+                    env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]=5 #there is no more water
             else :
-                print('mur')
                 next_state=state
                 reward=rewardlist[0]
 
@@ -120,6 +128,8 @@ def main(L, mouse_initial_indices, rewardlist, actions_list):
                 if steps % update_target == 0:
                     update_target_model(online_net, target_net)
 
+        if score>47:
+            print(score)
         running_score = 0.99 * running_score + 0.01 * score
         # running_score=score
         if e % log_interval == 0:
@@ -139,7 +149,7 @@ def test(L, mouse_initial_indices, rewardlist, actions_list):
     # L=np.array([[0,0,2,0,0,0,0],[0,1,1,0,1,1,0],[0,1,0,0,1,0,0],[0,1,1,1,1,0,0],[0,0,1,0,1,1,0],[0,0,0,0,0,1,0],[0,0,0,0,0,1,0],[0,0,0,0,0,0,0]])
     # mouse_initial_indices=[5,5]
 
-    env = L
+    env = deepcopy(L)
 
     done = False
     steps = 0
@@ -164,6 +174,8 @@ def test(L, mouse_initial_indices, rewardlist, actions_list):
             reward=rewardlist[env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]]
             if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]==2:
                 done=True
+            if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]==4: #if the mouse is in the water
+                env[int(new_state[0][0].tolist()),int(newstate[0][1].tolist())]=5 #there is no more water
         else :
             next_state=state
             reward=rewardlist[0]
@@ -185,21 +197,22 @@ def test(L, mouse_initial_indices, rewardlist, actions_list):
 
 
 if __name__=="__main__":
-    L = np.array(  [[0,0,0,0,0,0,0,0,0,0],
-                    [0,1,1,3,0,0,1,1,1,0],
-                    [0,0,1,1,1,1,1,0,1,0],
-                    [0,1,1,0,1,0,0,0,1,0],
-                    [0,1,0,0,1,1,1,0,1,0],
-                    [0,1,0,0,0,0,3,0,1,0],
-                    [0,1,0,1,1,0,1,0,1,0],
-                    [0,1,1,1,0,0,1,1,1,0],
-                    [0,0,0,1,0,0,0,1,0,0],
-                    [0,1,1,1,1,1,1,1,2,0],
-                    [0,0,0,0,0,0,0,0,0,0]
-                    ]).T
+    # L = np.array(  [[0,0,0,0,0,0,0,0,0,0],
+    #                 [0,1,1,3,0,0,1,1,1,0],
+    #                 [0,0,1,1,1,1,1,0,1,0],
+    #                 [0,1,1,0,1,0,0,0,1,0],
+    #                 [0,1,0,0,1,1,1,0,1,0],
+    #                 [0,1,0,0,0,0,3,0,1,0],
+    #                 [0,1,0,1,1,0,1,0,1,0],
+    #                 [0,1,1,1,0,0,1,1,1,0],
+    #                 [0,0,0,1,0,0,0,1,0,0],
+    #                 [0,1,1,1,1,1,1,1,2,0],
+    #                 [0,0,0,0,0,0,0,0,0,0]
+    #                 ]).T #Ne pas oublier de repasser l'exploration à 10000
+    L=np.array([[0,0,0,0,0],[0,1,1,3,0],[0,1,3,1,0],[0,1,1,2,0],[0,1,1,4,0],[0,0,0,0,0]]) #labyrinthe utilisé (0=mur, 1=vide, 2= arrivée, 3=électricité, 4=eau) #régler exploration 300 si seprendrelesmurs = False, 1000 sinon (dans config.py)
     mouse_initial_indices=[1,1]
-    rewardlist=[-5,-1,50,-10,5,-10] #se prendre un mur, se déplacer, arriver au fromage, se prendre l'électricité, boire de l'eau, revenir sur de l'eau
+    rewardlist=[-5,-1,50,-10,20,-10] #se prendre un mur, se déplacer, arriver au fromage, se prendre l'électricité, boire de l'eau, revenir sur de l'eau
     actions_list=[[1,0],[-1,0],[0,1],[0,-1]]
 
-    m=main(L, mouse_initial_indices, rewardlist, actions_list)
+    # m=main(L, mouse_initial_indices, rewardlist, actions_list,False)
     test(L, mouse_initial_indices, rewardlist, actions_list)
