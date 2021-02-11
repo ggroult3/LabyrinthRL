4df7968daa57d180f09f2eeaf7eef3d4ee53b7f4
Antoine Valentin <45234999+Orizno@users.noreply.github.com>
2021-02-11 11:01:26 +0100

    Rajout d'une nouvelle version du code qui marche à peu près avec une source d'eau

---
+++ Nouveau Qlearning/eau qui marche à peu près/config.py
@@ -0,0 +1,11 @@
+import torch
+
+gamma = 0.99
+batch_size = 32
+lr = 0.00003
+initial_exploration = 3000
+goal_score = 200
+log_interval = 10
+update_target = 100
+replay_memory_capacity = 1000
+device = torch.device("cuda")
