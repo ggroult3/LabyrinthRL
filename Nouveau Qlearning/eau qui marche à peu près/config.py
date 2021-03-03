c037f7d46cac9ce3928e40e320ac76ad6a946d6d
JVanaret <julesvanaret@outlook.fr>
2021-03-03 09:24:39 +0100

    rajout d'une ligne pour les configs sans cpu

--- Nouveau Qlearning/eau qui marche à peu près/config.py
+++ Nouveau Qlearning/eau qui marche à peu près/config.py
@@ -5,7 +5,9 @@ batch_size = 32
 lr = 0.00003
 initial_exploration = 3000
 goal_score = 200
-log_interval = 10
+log_interval = 20
 update_target = 100
 replay_memory_capacity = 1000
-device = torch.device("cuda")
+
+#device = torch.device("cuda")
+device = torch.device("cpu")
