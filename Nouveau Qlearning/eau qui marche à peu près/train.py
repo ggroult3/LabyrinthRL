de8bc6136d479277e0558a4cc6a0020111abc6e8
JVanaret <julesvanaret@outlook.fr>
2021-03-03 09:26:23 +0100

    ajout d'un graphe du score à la fin de la phase de train

--- Nouveau Qlearning/eau qui marche à peu près/train.py
+++ Nouveau Qlearning/eau qui marche à peu près/train.py
@@ -1,16 +1,19 @@
 import os
 import sys
-import gym
+import matplotlib.pyplot as plt
 import random
 import numpy as np
 from copy import deepcopy
 import torch
 import torch.optim as optim
-import torch.nn.functional as F
 from model import QNet
-from memory import Memory
-# from tensorboardX import SummaryWriter
+from memory import Memory 
 from display import Displayer
+
+if False:
+    import gym
+    from tensorboardX import SummaryWriter
+    import torch.nn.functional as F
 
 
 from config import initial_exploration, batch_size, update_target, goal_score, log_interval, device, replay_memory_capacity, lr
@@ -58,6 +61,9 @@ def update_target_model(online_net, target_net):
 
 
 def main(L, mouse_initial_indices, rewardlist, actions_list,seprendrelesmurs=True):
+    
+    scores = [0]
+    best_scores = [0]
 
     env = deepcopy(L)
     torch.manual_seed(2020)
@@ -83,8 +89,12 @@ def main(L, mouse_initial_indices, rewardlist, actions_list,seprendrelesmurs=Tru
     epsilon = 1.0
     steps = 0
     loss = 0
+    
+    best_score=0
 
-    for e in range(10000):
+
+    number_episode = 5000
+    for e in range(number_episode):
         done = False
         env = deepcopy(L)
         eaubue=0.
@@ -92,9 +102,15 @@ def main(L, mouse_initial_indices, rewardlist, actions_list,seprendrelesmurs=Tru
         state = np.array(mouse_initial_indices)
         state = torch.Tensor(state).to(device)
         state = state.unsqueeze(0)
+        
+        this_episode_step = 0
 
         while not done:
             steps += 1
+#            this_episode_step+=1
+            
+#            if this_episode_step > 10000:
+#                done=True
 
             action = get_action(state, target_net, epsilon, env,seprendrelesmurs=seprendrelesmurs,eaubue=eaubue)
             newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
@@ -129,23 +145,29 @@ def main(L, mouse_initial_indices, rewardlist, actions_list,seprendrelesmurs=Tru
                 batch = memory.sample(batch_size)
                 loss = QNet.train_model(online_net, target_net, optimizer, batch)
 
-
                 if steps % update_target == 0:
                     update_target_model(online_net, target_net)
 
-        # if score>47:
-        #     print(score)
         running_score = 0.99 * running_score + 0.01 * score
-        # running_score=score
+        #running_score = score
+        
+        scores.append(running_score)
+        best_scores.append(score if score > best_scores[-1] else best_scores[-1])
+        
         if e % log_interval == 0:
-            print('{} episode | score: {:.2f} | epsilon: {:.2f}'.format(
-                e, running_score, epsilon))
+            print('{} episode | score: {:.2f} | best score: {:.2f} | epsilon: {:.2f}'.format(
+                e, running_score, best_score, epsilon))
             # writer.add_scalar('log/score', float(running_score), e)
             # writer.add_scalar('log/loss', float(loss), e)
-            torch.save(online_net.state_dict(), "./qlearning_model")
+            
+            if score > best_score:
+                best_score = score
+                torch.save(online_net.state_dict(), "./qlearning_model")
 
         if running_score > goal_score:
             break
+
+    return number_episode, scores, best_scores
 
 
 def test(L, mouse_initial_indices, rewardlist, actions_list,seprendrelesmurs):
@@ -154,12 +176,13 @@ def test(L, mouse_initial_indices, rewardlist, actions_list,seprendrelesmurs):
     env = deepcopy(L)
 
     done = False
-    eaubue=0.
+    eaubue=0
     steps = 0
     score = 0
     state = np.array(mouse_initial_indices)
     state = torch.Tensor(state).to(device)
     state = state.unsqueeze(0)
+    
     def progress_loop(done, steps, state, score,eaubue):
         steps += 1
 
@@ -190,33 +213,59 @@ def test(L, mouse_initial_indices, rewardlist, actions_list,seprendrelesmurs):
         print('position : ',state.tolist()[0],score)
 
         if done is False:
-            displayer.window.after(100, lambda: progress_loop(done, steps, state, score,eaubue))
+            displayer.window.after(800, lambda: progress_loop(done, steps, state, score,eaubue))
     displayer = Displayer()
 
     displayer.create_labyrinth(L, mouse_initial_indices)
-    progress_loop(done, steps, state, score,0.)
+    progress_loop(done, steps, state, score,eaubue)
 
     displayer.window.mainloop()
+    
+    
+def plot_convergence(number_episodes, scores, best_scores):
+    
+    plt.plot(np.linspace(1, number_episodes, len(scores)), scores, lw=1)
+    plt.plot(np.linspace(1, number_episodes, len(best_scores)), best_scores, 'k', label='best')
+    
+    plt.xlim(1, number_episodes)
+    plt.ylim(max(-40, 1.2*min(scores)), 1.2*max(best_scores))
+    
+    plt.xlabel('episode', fontsize=15)
+    plt.ylabel('score', fontsize=15)
+    
+    plt.legend(prop={'size':15})
+    
+    
 
 
 
 if __name__=="__main__":
-    # L = np.array(  [[0,0,0,0,0,0,0,0,0,0],
-    #                 [0,1,1,3,0,0,1,1,1,0],
-    #                 [0,0,1,1,1,1,1,0,1,0],
-    #                 [0,1,1,0,1,0,0,0,1,0],
-    #                 [0,1,0,0,1,1,1,0,1,0],
-    #                 [0,1,0,0,0,0,3,0,1,0],
-    #                 [0,1,0,1,1,0,1,0,1,0],
-    #                 [0,1,1,1,0,0,1,1,1,0],
-    #                 [0,0,0,1,0,0,0,1,0,0],
-    #                 [0,1,1,1,1,1,1,1,2,0],
-    #                 [0,0,0,0,0,0,0,0,0,0]
-    #                 ]).T #Ne pas oublier de repasser l'exploration à 10000
+#    L = np.array(  [[0,0,0,0,0,0,0,0,0,0],
+#                     [0,1,1,3,0,0,1,1,1,0],
+#                     [0,0,1,1,1,1,1,0,1,0],
+#                     [0,1,1,0,1,0,0,0,1,0],
+#                     [0,1,0,0,1,1,1,0,1,0],
+#                     [0,1,0,0,0,0,3,0,1,0],
+#                     [0,1,0,1,1,0,1,0,1,0],
+#                     [0,1,1,1,0,0,1,1,1,0],
+#                     [0,0,0,1,0,0,0,1,0,0],
+#                     [0,1,1,1,1,1,1,1,2,0],
+#                     [0,0,0,0,0,0,0,0,0,0]
+#                     ]).T #Ne pas oublier de repasser l'exploration à 10000
+    
     L=np.array([[0,0,0,0,0],[0,1,1,3,0],[0,1,3,1,0],[0,1,1,2,0],[0,1,1,4,0],[0,0,0,0,0]]).T #labyrinth utilisé (0=mur, 1=vide, 2= arrivée, 3=électricité, 4=eau)
+    
     mouse_initial_indices=[1,1]
+    
     rewardlist=[-5,-1,50,-10,20,-10] #se prendre un mur, se déplacer, arriver au fromage, se prendre l'électricité, boire de l'eau, revenir sur de l'eau
     actions_list=[[1,0],[-1,0],[0,1],[0,-1]]
 
-    # m=main(L, mouse_initial_indices, rewardlist, actions_list,True)
-    test(L, mouse_initial_indices, rewardlist, actions_list,True)
+
+    testing = False
+    
+    if not testing:
+        number_episodes, scores, best_scores = main(L, mouse_initial_indices, rewardlist, actions_list,True)
+        plot_convergence(number_episodes, scores, best_scores)
+        
+    else:
+        test(L, mouse_initial_indices, rewardlist, actions_list,seprendrelesmurs=True)
