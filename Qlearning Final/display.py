cc3c18f7194354009dc7f23da2b6e98b8596891b
Antoine Valentin <45234999+Orizno@users.noreply.github.com>
2021-03-24 18:03:17 +0100

    Q Learning final

---
+++ Qlearning Final/display.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+import tkinter as tk
+
+
+class Displayer():
+    def __init__(self):
+        self.WIDTH = 500
+        self.HEIGHT = 500
+        
+    def _make_square_box(self, i, j, mouse=False):
+        """
+        Make square bounding box from indices.
+        If mouse is True, the bounding box produced is smaller.
+        """
+        
+        if mouse is True:
+            return [self.square_size * elem for elem in ((i+0.2), (j+0.2), (i+0.8), (j+0.8))]
+        else: 
+            return [self.square_size * elem for elem in (i, j, i+1, j+1)]
+        
+        
+    def create_labyrinth(self, matrix_labyrinth, mouse_initial_indices):
+        
+        self.matrix = matrix_labyrinth
+        self.square_size = self.WIDTH // (np.max(self.matrix.shape)+1) 
+        
+        self.window = tk.Tk()
+        self.window.title('labyrinth')
+        
+        
+        self.window.geometry('%sx%s'%(self.WIDTH,self.HEIGHT))
+        self.main_canva = tk.Canvas(self.window, width=self.WIDTH, height=self.HEIGHT-30, bg='white')
+        self.main_canva.pack()
+        self.main_canva.focus_set()
+        
+        for i in range(np.shape(self.matrix)[0]):
+            for j in range(np.shape(self.matrix)[1]):
+                
+                square_box = self._make_square_box(i,j)
+                
+                if self.matrix[i,j] == 0:
+                    self.main_canva.create_rectangle(*square_box, fill="black")
+                
+                if self.matrix[i,j] == 1:
+                    self.main_canva.create_rectangle(*square_box, fill="white")
+                            
+                if self.matrix[i,j] == 2:
+                    self.main_canva.create_rectangle(*square_box, fill="green")
+            
+                if self.matrix[i,j] == 3:
+                    self.main_canva.create_rectangle(*square_box, fill="red")
+                
+                if self.matrix[i,j] == 4:
+                    self.main_canva.create_rectangle(*square_box, fill="blue")
+
+
+        self.mouse = self.main_canva.create_rectangle(
+            self._make_square_box(*mouse_initial_indices, mouse=True),
+            fill="grey"
+        )
+
+if __name__=="__main__":
+    
+    L=np.array([[0,0,0,0,0],[0,1,1,3,0],[0,1,3,1,0],[0,1,1,2,0],[0,1,1,4,0],[0,0,0,0,0]]).T #labyrinth utilisé (0=mur, 1=vide, 2= arrivée, 3=électricité, 4=eau)
+    mouse_initial_indices=[1,1]
+    displayer = Displayer()
+    displayer.create_labyrinth(L, mouse_initial_indices)
+    displayer.window.mainloop()
+    
+    
---
+++ Qlearning Final/train.py
@@ -0,0 +1,231 @@
+import os
+import sys
+import random
+import numpy as np
+from copy import deepcopy
+import torch
+import torch.optim as optim
+#import torch.nn.functional as F
+from model import QNet
+from memory import Memory
+# from tensorboardX import SummaryWriter
+from display import Displayer
+import matplotlib.pyplot as plt
+
+
+from config import initial_exploration, batch_size, update_target, goal_score, log_interval, device, replay_memory_capacity, lr
+
+def get_action(state, target_net, epsilon, env,test=False,eaubue=0.):
+    stateaugmente=torch.cat((state,torch.tensor(eaubue).unsqueeze(0).unsqueeze(0).to(device)),1)
+
+    if np.random.rand() <= epsilon and test==False:
+        return torch.tensor([[random.randrange(4)]], device=device, dtype=torch.long)
+    else:
+        _, action = torch.max(target_net.get_action(stateaugmente), 1)
+        return action.cpu().numpy()[0]
+
+def update_target_model(online_net, target_net):
+    # Target <- Net
+    target_net.load_state_dict(online_net.state_dict())
+
+
+def main(L, mouse_initial_indices, rewardlist, actions_list):
+    if mouse_initial_indices is None:
+        all_possible_starting_positions = np.array([*np.where(L==1)]).T
+    scores = [0]
+    best_scores = [0]
+    env = deepcopy(L)
+    torch.manual_seed(2020)
+
+    num_inputs = 2+1
+    num_actions = 4
+    print('state size:', num_inputs)
+    print('action size:', num_actions)
+
+    online_net = QNet(num_inputs, num_actions)
+    target_net = QNet(num_inputs, num_actions)
+    update_target_model(online_net, target_net)
+
+    optimizer = optim.Adam(online_net.parameters(), lr=lr)
+    # writer = SummaryWriter('logs')
+
+    online_net.to(device)
+    target_net.to(device)
+    online_net.train()
+    target_net.train()
+    memory = Memory(replay_memory_capacity)
+    running_score = 0
+    epsilon = 1.0
+    steps = 0
+    loss = 0
+    inint=mouse_initial_indices
+    best_score=0
+    number_episode = 1000
+    for e in range(number_episode):
+        if inint is None:
+            mouse_initial_indices = all_possible_starting_positions[
+                                        np.random.choice(range(len(all_possible_starting_positions)))
+                                    ]
+
+        done = False
+        env = deepcopy(L)
+        eaubue=0.
+        score = 0
+        state = np.array(mouse_initial_indices)
+        state = torch.Tensor(state).to(device)
+        state = state.unsqueeze(0)
+
+        while not done:
+            steps += 1
+
+            action = get_action(state, target_net, epsilon, env,eaubue=eaubue)
+            newstate=state+torch.Tensor(np.array(actions_list[action])).to(device)
+            if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]!=0:
+                next_state=newstate
+                new_eaubue=eaubue
+                reward=rewardlist[env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]]
+                if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]==2:
+                    done=True
+                if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]==4: #if the mouse is in the water
+                    env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]=5 #there is no more water
+                    new_eaubue=1.
+            else :
+                next_state=state
+                reward=rewardlist[0]
+                new_eaubue=eaubue
+
+
+            mask = 0 if done else 1
+            action_one_hot = np.zeros(4)
+            action_one_hot[action] = 1
+            memory.push(torch.cat((state,torch.tensor(eaubue).unsqueeze(0).unsqueeze(0).to(device)),1), torch.cat((next_state,torch.tensor(new_eaubue).unsqueeze(0).unsqueeze(0).to(device)),1), action_one_hot, reward, mask)
+
+            score += reward
+            state = next_state
+            eaubue=new_eaubue
+
+            if steps > initial_exploration:
+                epsilon -= 0.00005
+                epsilon = max(epsilon, 0.1)
+
+                batch = memory.sample(batch_size)
+                loss = QNet.train_model(online_net, target_net, optimizer, batch)
+
+
+                if steps % update_target == 0:
+                    update_target_model(online_net, target_net)
+
+        # print("OK")
+        if score>35:
+            print(score)
+        running_score = 0.99 * running_score + 0.01 * score
+        # running_score=score
+        scores.append(running_score)
+        best_scores.append(score if score > best_scores[-1] else best_scores[-1])
+        if e % log_interval == 0:
+            print('{} episode | score: {:.2f} | best score: {:.2f} | epsilon: {:.2f}'.format(
+                e, running_score, best_score, epsilon))
+            # writer.add_scalar('log/score', float(running_score), e)
+            # writer.add_scalar('log/loss', float(loss), e)
+            if score > best_score:
+                best_score = score
+            torch.save(online_net.state_dict(), "./qlearning_model")
+
+        if running_score > goal_score:
+            break
+
+    return number_episode, scores, best_scores
+
+def test(L, mouse_initial_indices, rewardlist, actions_list):
+    online_net = QNet(3, 4).to(device)
+    online_net.load_state_dict(torch.load("./qlearning_model", map_location=device))
+    env = deepcopy(L)
+
+    done = False
+    eaubue=0.
+    steps = 0
+    score = 0
+    if mouse_initial_indices is None:
+        all_possible_starting_positions = np.array([*np.where(L==1)]).T
+        mouse_initial_indices = all_possible_starting_positions[
+                                        np.random.choice(range(len(all_possible_starting_positions)))
+                                    ]
+    state = np.array(mouse_initial_indices)
+    state = torch.Tensor(state).to(device)
+    state = state.unsqueeze(0)
+    def progress_loop(done, steps, state, score,eaubue):
+        steps += 1
+
+        action = get_action(state, online_net, 1, env,True,eaubue)
+        displacement = np.array(actions_list[action])
+        newstate=state+torch.Tensor(displacement).to(device)
+        if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]!=0:
+            next_state=newstate
+
+            displayer.main_canva.move(
+                displayer.mouse,
+                *(displacement  * displayer.square_size)
+            )
+
+            reward=rewardlist[env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]]
+            if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]==2:
+                done=True
+            if env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]==4: #if the mouse is in the water
+                env[int(newstate[0][0].tolist()),int(newstate[0][1].tolist())]=5 #there is no more water
+                eaubue=1.
+        else :
+            next_state=state
+            reward=rewardlist[0]
+
+
+        score += reward
+        state = next_state
+        print('position : ',state.tolist()[0],score)
+
+        if done is False:
+            displayer.window.after(800, lambda: progress_loop(done, steps, state, score,eaubue))
+    displayer = Displayer()
+
+    displayer.create_labyrinth(L, mouse_initial_indices)
+    progress_loop(done, steps, state, score,0.)
+
+    displayer.window.mainloop()
+
+def plot_convergence(number_episodes, scores, best_scores):
+
+    plt.plot(np.linspace(1, number_episodes, len(scores)), scores, lw=1)
+    plt.plot(np.linspace(1, number_episodes, len(best_scores)), best_scores, 'k', label='best')
+
+    plt.xlim(1, number_episodes)
+    plt.ylim(max(-40, 1.2*min(scores)), 1.2*max(best_scores))
+
+    plt.xlabel('episode', fontsize=15)
+    plt.ylabel('score', fontsize=15)
+
+    plt.legend(prop={'size':15})
+    plt.show()
+
+if __name__=="__main__":
+    L = np.array(  [[0,0,0,0,0,0,0,0,0,0],
+                    [0,1,1,3,0,0,1,1,1,0],
+                    [0,0,1,1,1,1,1,0,4,0],
+                    [0,1,1,0,1,0,0,0,1,0],
+                    [0,1,0,0,1,1,1,0,1,0],
+                    [0,1,0,0,0,0,3,0,1,0],
+                    [0,1,0,1,1,0,1,0,1,0],
+                    [0,1,1,1,0,0,1,1,1,0],
+                    [0,0,0,1,0,0,0,1,0,0],
+                    [0,1,1,1,1,1,1,1,2,0],
+                    [0,0,0,0,0,0,0,0,0,0]
+                    ]).T
+    # L=np.array([[0,0,0,0,0],[0,1,1,3,0],[0,1,3,1,0],[0,1,1,2,0],[0,1,1,4,0],[0,0,0,0,0]]).T #labyrinth utilisé (0=mur, 1=vide, 2= arrivée, 3=électricité, 4=eau)
+    mouse_initial_indices=[1,1]
+    rewardlist=[-5,-1,50,-10,20,-10] #se prendre un mur, se déplacer, arriver au fromage, se prendre l'électricité, boire de l'eau, revenir sur de l'eau
+    actions_list=[[1,0],[-1,0],[0,1],[0,-1]]
+    testing = True
+
+    if not testing:
+        number_episodes, scores, best_scores =main(L, mouse_initial_indices, rewardlist, actions_list)
+        plot_convergence(number_episodes, scores, best_scores)
+    else:
+        test(L, mouse_initial_indices, rewardlist, actions_list)
